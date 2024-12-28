56f19f0b035a3cb23eb560eb6e5b30dd68832701
David Moll <dlmoll@proton.me>
2024-12-28 21:32:10 +0100

    Small cleanup

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,46 +1,50 @@
 const htmlNano = require('htmlnano');
 
 module.exports = function (eleventyConfig) {
-        eleventyConfig.addPassthroughCopy("src/output.css");
+    eleventyConfig.addPassthroughCopy("src/output.css");
 
-        eleventyConfig.addFilter("sortSkill", function (collection){
-            return collection.sort((a, b) => b.data.experience - a.data.experience);
-          })
+    eleventyConfig.addFilter("sortSkill", function (collection) {
+        return collection.sort((a, b) => b.data.experience - a.data.experience);
+    })
 
-        eleventyConfig.addShortcode("currentYear", () => {
-           return new Date().getUTCFullYear()
-        });
+    eleventyConfig.addFilter("sortByYear", function (collection) {
+        return collection.sort((a, b) => b.data.start - a.data.start);
+    })
 
-        eleventyConfig.addShortcode("yearsSince", () => {
-            const currentDate = new Date();
-            const targetDate = new Date("2021-11-15");
-            const diffTime = currentDate - targetDate;
+    eleventyConfig.addShortcode("currentYear", () => {
+        return new Date().getUTCFullYear()
+    });
 
-            const yearsDifference = diffTime / (1000 * 60 * 60 * 24 * 365.25);
-            return Math.floor(yearsDifference);
-        });
-        
-        eleventyConfig.addFilter("formatToLocateDate", function(value) {
-            if (value instanceof Date) {
-                return value.toLocaleDateString('de-DE', { day: '2-digit', month: '2-digit', year: 'numeric' });
-            }
-            return value;
-        });
+    eleventyConfig.addShortcode("yearsSince", () => {
+        const currentDate = new Date();
+        const targetDate = new Date("2021-11-15");
+        const diffTime = currentDate - targetDate;
 
-        eleventyConfig.addTransform('htmlnano', async (content, outputPath) => {
-            if (outputPath.endsWith('.html')) {
-                const { html } = await htmlNano.process(content, {
-                    collapseBooleanAttributes: true,
-                    collapseWhitespace: true,
-                    removeComments: true,
-                    removeEmptyAttributes: true,
-                    removeRedundantAttributes: true,
-                });
-                return html;
-            }
-            return content;
-        });
-        
+        const yearsDifference = diffTime / (1000 * 60 * 60 * 24 * 365.25);
+        return Math.floor(yearsDifference);
+    });
+
+    eleventyConfig.addFilter("formatToLocateDate", function (value) {
+        if (value instanceof Date) {
+            return value.toLocaleDateString('de-DE', { day: '2-digit', month: '2-digit', year: 'numeric' });
+        }
+        return value;
+    });
+
+    eleventyConfig.addTransform('htmlnano', async (content, outputPath) => {
+        if (outputPath.endsWith('.html')) {
+            const { html } = await htmlNano.process(content, {
+                collapseBooleanAttributes: true,
+                collapseWhitespace: true,
+                removeComments: true,
+                removeEmptyAttributes: true,
+                removeRedundantAttributes: true,
+            });
+            return html;
+        }
+        return content;
+    });
+
     return {
         dir: {
             input: 'src',
