75d0b8e25795fcf158f27f1877751e6388e46592
David Moll <dlmoll@proton.me>
2025-06-13 14:11:07 +0200

    Minimize all the files

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,6 @@
 import fontAwesomePlugin from "@11ty/font-awesome";
+import CleanCSS from "clean-css";
+import { minify } from "terser";
 
 export default async function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("bundle.css");
@@ -6,4 +8,18 @@ export default async function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("assets/");
 
     eleventyConfig.addPlugin(fontAwesomePlugin);
+
+    eleventyConfig.addFilter("cssmin", function (code) {
+		return new CleanCSS({}).minify(code).styles;
+	});
+
+eleventyConfig.addNunjucksAsyncFilter("jsmin", async function (code, callback) {
+  try {
+    const minified = await minify(code);
+    callback(null, minified.code);
+  } catch (err) {
+    console.error("Terser error: ", err);
+    callback(null, code);
+  }
+});
 };
